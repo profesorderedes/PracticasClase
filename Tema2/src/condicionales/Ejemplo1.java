b9bb29f084fd9c66552a4df19ce9d674b0bd085f
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-23 16:51:19 +0200

    vie 23 oct 2020 16:51:19 CEST

---
+++ Tema2/src/condicionales/Ejemplo1.java
@@ -0,0 +1,18 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("¿Qué sueldo tienes?");
+		
+		
+		
+		
+	}
+
+}
