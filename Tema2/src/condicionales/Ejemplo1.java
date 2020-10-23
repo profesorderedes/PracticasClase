a9cb5758d75d3db10b6bff2a181c90cc3339a7b5
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-23 16:59:04 +0200

    vie 23 oct 2020 16:59:04 CEST

--- Tema2/src/condicionales/Ejemplo1.java
+++ Tema2/src/condicionales/Ejemplo1.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Ejemplo1 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
 
 		System.out.println("¿Qué sueldo tienes?");
-		
-		
-		
-		
+		float sueldo = entrada.nextFloat();
+
+		// Si el sueldo es mayor de 1500, el programa lo considerará bueno.
+		if (sueldo > 1500) {
+			System.out.println("Me parece un buen sueldo.");
+			System.out.println("Enhorabuena.");
+		}
+
+		System.out.println("Gracias.");
+
 	}
 
 }
