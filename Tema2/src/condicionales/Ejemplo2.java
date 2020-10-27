271a25684de57241ad853489e76d1ffa5f34a232
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-27 18:55:49 +0100

    mar 27 oct 2020 18:55:49 CET

--- Tema2/src/condicionales/Ejemplo2.java
+++ Tema2/src/condicionales/Ejemplo2.java
@@ -1,10 +1,25 @@
 package condicionales;
 
+import java.util.Scanner;
+
 public class Ejemplo2 {
 
 	public static void main(String[] args) {
 		
+		//double cantidadParaDescuento = 50;
+		Scanner entrada = new Scanner(System.in);
 		
+		System.out.print("Precio: ");
+		double precio = entrada.nextDouble();
+		
+		if(precio > 50) {
+			
+			// Haremos descuento de 10%.
+			precio = precio - (precio * 10 /100);
+			
+		}
+		
+		System.out.println("El precio a pagar es " + precio + " €.");
 
 	}
 
