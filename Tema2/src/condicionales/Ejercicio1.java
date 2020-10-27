0ce90f720b3adf3bafcfa1c21eea874b181e3863
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-27 19:52:49 +0100

    mar 27 oct 2020 19:52:49 CET

---
+++ Tema2/src/condicionales/Ejercicio1.java
@@ -0,0 +1,23 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Introduce la temperatura del reactor:");
+		float temperatura = entrada.nextFloat();
+
+		if (temperatura > 120) {
+
+			System.out.println("TEMPERATURA DEL REACTOR CRÍTICA. "
+					+ "¡CORRED, INSENSATOS!");
+
+		}
+
+	}
+
+}
---
+++ Tema2/src/condicionales/Ejercicio2.java
@@ -0,0 +1,13 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
