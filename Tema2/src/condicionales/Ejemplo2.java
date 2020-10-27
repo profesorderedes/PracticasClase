3dc4886d42481626bf79db702c91b9f94d74c16c
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-27 19:27:19 +0100

    mar 27 oct 2020 19:27:19 CET

--- Tema2/src/condicionales/Ejemplo2.java
+++ Tema2/src/condicionales/Ejemplo2.java
@@ -6,17 +6,19 @@ public class Ejemplo2 {
 
 	public static void main(String[] args) {
 		
-		//double cantidadParaDescuento = 50;
+		final double CANTIDAD_PARA_DESCUENTO = 50;
+		final double PORCENTAJE_DESCUENTO = 10;
+		
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.print("Precio: ");
 		double precio = entrada.nextDouble();
 		
-		if(precio > 50) {
+		if(precio > CANTIDAD_PARA_DESCUENTO) {
 			
 			// Haremos descuento de 10%.
 			System.out.println("Tienes derecho a descuento.");
-			precio = precio - (precio * 10 /100);
+			precio = precio - (precio * PORCENTAJE_DESCUENTO /100);
 			
 		}else {
 			System.out.println("No tienes derecho a un descuento.");
