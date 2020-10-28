d1cab9541013a60117846d4df13118eb5b70cfe3
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-28 20:41:38 +0100

    mié 28 oct 2020 20:41:38 CET

--- Tema2/src/condicionales/Ejercicio9.java
+++ Tema2/src/condicionales/Ejercicio9.java
@@ -11,7 +11,26 @@ public class Ejercicio9 {
 		System.out.print("Precio: ");
 		float precio = entrada.nextFloat();
 		
+		System.out.print("Unidades: ");
+		int unidades = entrada.nextInt();
 		
+		float total = precio * unidades;
+		
+//		if(total < 100) {
+//			System.out.println("No hay descuento.");
+//		} else if (total >= 100 && total <= 200) {
+//			total = total - total * 10 / 100;
+//		} else {
+//			total = total - total * 15 / 100;
+//		}
+		
+		if(total < 100) {
+			System.out.println("No hay descuento.");
+		} else if (total > 200) {
+			total = total - total * 15 / 100;
+		} else {
+			total = total - total * 10 / 100;
+		}
 
 	}
 
