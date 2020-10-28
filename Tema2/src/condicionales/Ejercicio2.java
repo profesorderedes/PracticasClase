1319975829419c9035e5721dbcc29b05c5a06887
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-28 19:29:31 +0100

    mié 28 oct 2020 19:29:31 CET

---
+++ Tema2/src/condicionales/Ejercicio2.java
@@ -0,0 +1,22 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Precio: ");
+		float precio = entrada.nextFloat();
+
+		if (precio > 80) {
+			precio = precio - precio * 10 / 100;
+		}
+
+		System.out.println("Precio final: " + precio + " €.");
+
+	}
+
+}
