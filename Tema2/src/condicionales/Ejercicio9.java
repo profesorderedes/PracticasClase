bab8337d5170e282518aac3cc8f99f233e04971d
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-28 20:31:58 +0100

    mié 28 oct 2020 20:31:58 CET

---
+++ Tema2/src/condicionales/Ejercicio9.java
@@ -0,0 +1,18 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Precio: ");
+		float precio = entrada.nextFloat();
+		
+		
+
+	}
+
+}
