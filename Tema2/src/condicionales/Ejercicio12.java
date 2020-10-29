a9b70f642a218309030a453259222ec0ac9dfeec
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-29 15:54:39 +0100

    jue 29 oct 2020 15:54:39 CET

---
+++ Tema2/src/condicionales/Ejercicio12.java
@@ -0,0 +1,26 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("¡Dime un número de mes!");
+		int mes = entrada.nextInt();
+		
+		if(mes == 1) {
+			System.out.println("Enero");
+		} else if (mes == 2) {
+			System.out.println("Febrero");
+		} else if (mes == 3) {
+			System.out.println("Marzo");
+		} else if (mes == 4) {
+			System.out.println("Abril");
+		}
+		
+	}
+	
+}
---
+++ Tema2/src/condicionales/Ejercicio13.java
@@ -0,0 +1,28 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio13 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("¡Dime un número de mes!");
+		int mes = entrada.nextInt();
+		
+		if(mes == 1) {
+			System.out.println("Enero");
+		} else if (mes == 2) {
+			System.out.println("Febrero");
+		} else if (mes == 3) {
+			System.out.println("Marzo");
+		} else if (mes == 4) {
+			System.out.println("Abril");
+		} else {
+			System.out.println("El número introducido debe estar entre 1 y 4.");
+		}
+		
+	}
+	
+}
