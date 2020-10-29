cf38db10cb6004e75032d8864a846253fab35eeb
Carlos Sogorb <profesorderedes@gmail.com>
2020-10-29 16:43:02 +0100

    jue 29 oct 2020 16:43:02 CET

---
+++ Tema2/src/condicionales/Ejercicio15.java
@@ -0,0 +1,58 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Escribe el número de mes:");
+		int mes = entrada.nextInt();
+
+		switch (mes) {
+
+		case 1:
+			System.out.println("");
+			break;
+		case 2:
+			System.out.println("");
+			break;
+		case 3:
+			System.out.println("");
+			break;
+		case 4:
+			System.out.println("");
+			break;
+		case 5:
+			System.out.println("");
+			break;
+		case 6:
+			System.out.println("");
+			break;
+		case 7:
+			System.out.println("");
+			break;
+		case 8:
+			System.out.println("");
+			break;
+		case 9:
+			System.out.println("");
+			break;
+		case 10:
+			System.out.println("");
+			break;
+		case 11:
+			System.out.println("");
+			break;
+		case 12:
+			System.out.println("");
+			break;
+		default:
+			System.out.println("");
+		}
+
+	}
+
+}
