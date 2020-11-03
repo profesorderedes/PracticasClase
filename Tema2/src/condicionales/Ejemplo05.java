184908a71aa0db526fca64ae5a69c58c966fdf9c
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-03 19:17:47 +0100

    mar 03 nov 2020 19:17:47 CET

--- Tema2/src/condicionales/Ejemplo05.java
+++ Tema2/src/condicionales/Ejemplo05.java
@@ -3,18 +3,29 @@ package condicionales;
 import java.util.Scanner;
 
 public class Ejemplo05 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
-		// Pedimos al usuario que escriba cúantos años tiene.
+
+		// Pedimos al usuario que escriba cuántos años tiene.
 		// Si se equivoca al escribir el dato, le informamos con
 		// un mensaje.
-		
-		
-		
-		
+		System.out.println("¿Cuántos años tienes?");
+
+		if (entrada.hasNextInt()) {
+			
+			// El usuario ha puesto un dato correcto.
+			int edad = entrada.nextInt();
+			System.out.println("Tienes " + edad + " años.");
+			
+		}else {
+			
+			System.out.println("La edad que has escrito no es correcta. "
+					+ "Debe ser un número entero.");
+			
+		}
+
 	}
 
 }
