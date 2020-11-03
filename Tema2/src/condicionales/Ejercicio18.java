c542b0a8b0fb011e6b2378a677316e3b1eb69686
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-03 19:46:28 +0100

    mar 03 nov 2020 19:46:28 CET

---
+++ Tema2/src/condicionales/Ejercicio18.java
@@ -0,0 +1,23 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio18 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Escribe un número entero entre 0 y 10");
+
+		if (entrada.hasNextInt()) {
+			int num = entrada.nextInt();
+			System.out.println("El número elegido es " + num);
+		} else {
+			System.out.println("Te he dicho que escribieras un número "
+					+ "entre 0 y 10.");
+		}
+
+	}
+
+}
---
+++ Tema2/src/condicionales/Ejercicio19.java
@@ -0,0 +1,33 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio19 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Adivina el número entero entre 0 y 10");
+
+		if (entrada.hasNextInt()) {
+			
+			int num = entrada.nextInt();
+			if(num == 3) {
+				
+				System.out.println("¡¡Has acertado el número secreto!!");
+				
+			}else {
+				
+				System.out.println("No has acertado el número secreto.");
+				
+			}
+			
+		} else {
+			System.out.println("Te he dicho que escribieras un número "
+					+ "entre 0 y 10.");
+		}
+
+	}
+
+}
