42d469b42e87b994906f422c6675b877ca66b869
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-03 19:12:38 +0100

    mar 03 nov 2020 19:12:38 CET

--- Tema2/src/condicionales/Ejemplo05.java
+++ Tema2/src/condicionales/Ejemplo05.java
@@ -1,5 +1,20 @@
 package condicionales;
 
+import java.util.Scanner;
+
 public class Ejemplo05 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		// Pedimos al usuario que escriba cúantos años tiene.
+		// Si se equivoca al escribir el dato, le informamos con
+		// un mensaje.
+		
+		
+		
+		
+	}
 
 }
