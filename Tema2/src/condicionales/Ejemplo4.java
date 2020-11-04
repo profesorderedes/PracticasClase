d88b0a1fe4769e8e705d5432b4fbd099571cdc84
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-04 19:35:21 +0100

    mié 04 nov 2020 19:35:21 CET

--- Tema2/src/condicionales/Ejemplo4.java
+++ Tema2/src/condicionales/Ejemplo4.java
@@ -1,12 +1,32 @@
 package condicionales;
 
+import java.util.Scanner;
+
 // Ejemplo de operador ternario
+// Portero de discoteca
 public class Ejemplo4 {
 
 	public static void main(String[] args) {
 
-		
-		
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("¿Qué edad tienes?");
+		int edad = entrada.nextInt();
+
+		String respuesta;
+
+		// variable = condición?valor_si_es_cierta:valor_si_es_falsa;
+
+		respuesta = edad >= 18 ? "Puedes pasar." : "No puedes pasar.";
+
+//		if(edad >= 18) {
+//			respuesta = "Puedes pasar.";
+//		} else {
+//			respuesta = "No puedes pasar.";
+//		}
+
+		System.out.println(respuesta);
+
 	}
 
 }
