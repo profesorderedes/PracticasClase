08050fdd8d6b4e29338838258736c5662142a74a
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-06 16:26:13 +0100

    vie 06 nov 2020 16:26:13 CET

---
+++ Tema2/src/bucles/Ejemplo08.java
@@ -0,0 +1,18 @@
+package bucles;
+
+public class Ejemplo08 {
+
+	public static void main(String[] args) {
+
+		int contador = 1;
+		
+		do {
+			
+			System.out.println(contador);
+			contador++;
+			
+		} while (contador <= 100);
+		
+	}
+
+}
