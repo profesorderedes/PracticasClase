5be453be11838ac942385a662ce9b7d853a35d89
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-10 19:27:21 +0100

    mar 10 nov 2020 19:27:21 CET

---
+++ Tema2/src/Ejercicio13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio13 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Escribe un número:");
+		int num = entrada.nextInt();
+
+		int resultado;
+		
+		for (int i = 1; i <= 10; i++) {
+
+			resultado = num * i;
+			System.out.println(num + " x " + i + " = " + resultado);
+
+		}
+
+	}
+}
