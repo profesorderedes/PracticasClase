0c646e519538d87826f2901fc8aff2f688a57f12
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-10 18:42:42 +0100

    mar 10 nov 2020 18:42:42 CET

---
+++ Tema2/src/bucles/Ejercicio12.java
@@ -0,0 +1,29 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		int num;
+
+		do {
+
+			System.out.println("¿Quien es el autor de la novela Ready player one?");
+			System.out.println("1. Dougles Capland");
+			System.out.println("2. Isaac Asimov");
+			System.out.println("3. Ernest Cline");
+			System.out.println("4. Ray Brandburry");
+
+			num = entrada.nextInt();
+
+		} while (num < 1 || num > 4);
+
+		System.out.println("Has acertado");
+
+	}
+
+}
