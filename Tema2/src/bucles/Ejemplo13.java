eeafa1e4e1b7bdc7ebe309d956501e054bb1ba8d
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-11 19:54:30 +0100

    mié 11 nov 2020 19:54:30 CET

---
+++ Tema2/src/bucles/Ejemplo13.java
@@ -0,0 +1,39 @@
+package bucles;
+
+import java.util.Scanner;
+
+// Programa que pregunta 5 números al usuario y, al acabar, le indica
+// cuántos han sido positivos.
+
+public class Ejemplo13 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int num;
+		int positivos = 0;
+		
+		for(int i = 1; i<=5; i++) {
+			
+			System.out.print("Número " + i + ": ");
+			num = entrada.nextInt();
+			
+			if(num > 0) {
+				
+			}
+			
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
--- Tema2/src/bucles/Ejercicio15.java
+++ Tema2/src/bucles/Ejercicio15.java
@@ -3,13 +3,24 @@ package bucles;
 import java.util.Scanner;
 
 public class Ejercicio15 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
+
+		float precio, suma = 0;
+
+		for (int i = 1; i <= 4; i++) {
+
+			System.out.print("Precio: ");
+			precio = entrada.nextFloat();
+
+			suma = suma + precio;
+
+		}
 		
-		
-		
+		System.out.println("Precio total: " + suma + " €.");
+
 	}
 
 }
