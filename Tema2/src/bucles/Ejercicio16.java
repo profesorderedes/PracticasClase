ab4e137fa9bed5c735f5adbcc65e9763e5c91358
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-11 20:08:45 +0100

    mié 11 nov 2020 20:08:45 CET

---
+++ Tema2/src/bucles/Ejercicio16.java
@@ -0,0 +1,19 @@
+package bucles;
+
+public class Ejercicio16 {
+	
+	public static void main(String[] args) {
+		
+		int potencia = 2;
+		
+		for(int i = 1; i <= 10; i++) {
+			
+			System.out.println(potencia);
+			potencia = potencia * 2;
+			
+			
+		}
+		
+	}
+
+}
