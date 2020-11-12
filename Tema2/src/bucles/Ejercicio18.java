85f4da6efc1cea428cb10e2784305f02788bd90c
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-12 16:06:10 +0100

    jue 12 nov 2020 16:06:10 CET

---
+++ Tema2/src/bucles/Ejercicio18.java
@@ -0,0 +1,21 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio18 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		double nota;
+		for (int i = 1; i <= 5; i++) {
+
+			System.out.print("Nota? ");
+			nota = entrada.nextDouble();
+
+		}
+
+	}
+
+}
