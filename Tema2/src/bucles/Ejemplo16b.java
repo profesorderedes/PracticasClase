c827f82b6a03fffd6f6a2674131186138bb1c091
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-17 20:21:12 +0100

    mar 17 nov 2020 20:21:12 CET

---
+++ Tema2/src/bucles/Ejemplo16b.java
@@ -0,0 +1,28 @@
+package bucles;
+
+// Ejemplo sin continue.
+
+public class Ejemplo16b {
+
+	public static void main(String[] args) {
+
+		// Programa que muestra los múltiplos de 5 hasta el 500,
+		// pero no los múltiplos de 100.
+
+		int i = 0;
+
+		while (i < 500) {
+
+			i = i + 5;
+
+			if (i % 100 != 0) {
+				System.out.println(i);
+			}
+
+		}
+
+		System.out.println("De nada.");
+
+	}
+
+}
