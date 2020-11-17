2085b7e41f4c251b0273a3f8459ae7b7551f0b23
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-17 19:10:41 +0100

    mar 17 nov 2020 19:10:41 CET

--- Tema2/src/bucles/Ejemplo14.java
+++ Tema2/src/bucles/Ejemplo14.java
@@ -5,26 +5,30 @@ import java.util.Scanner;
 // Ejemplo de break.
 
 public class Ejemplo14 {
-	
+
 	public static void main(String[] args) {
-		
+
 		// Programa que va pidiendo nombres hasta que el usuario
 		// escribe "parar".
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("Nombre: ");
 		String nombre = entrada.nextLine();
-		
-		while(!nombre.equals("parar")) {
-			
+
+		while (true) {
+
+			if (nombre.equals("parar")) {
+				break;
+			}
+
 			System.out.println("Nombre: ");
 			nombre = entrada.nextLine();
-			
+
 		}
-		
+
 		System.out.println("Por fin has escrito parar.");
-		
+
 	}
 
 }
---
+++ Tema2/src/bucles/Ejemplo15.java
@@ -0,0 +1,42 @@
+package bucles;
+
+import java.util.Scanner;
+
+// Ejemplo de break.
+
+public class Ejemplo15 {
+
+	public static void main(String[] args) {
+		
+		// Programa que vaya pidiendo números y sumándolos.
+		// Parará cuando el usuario escriba un 0.
+		// Y mostrará el total.
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		float cantidad;
+		
+		while(true) {
+			
+			System.out.print("Cantidad: ");
+			cantidad = entrada.nextFloat();
+		
+			
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
