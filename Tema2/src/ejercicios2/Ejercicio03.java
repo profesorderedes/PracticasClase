f58bd83b36e2f7c3e48915d98bab5f3dcadc706d
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-19 15:51:42 +0100

    jue 19 nov 2020 15:51:42 CET

--- Tema2/src/ejercicios2/Ejercicio03.java
+++ Tema2/src/ejercicios2/Ejercicio03.java
@@ -6,13 +6,12 @@ public class Ejercicio03 {
 
 		int i = 0;
 		double potencia = 1;
-		
+
 		do {
-			
-			i++;
+
 			System.out.println(potencia);
-			potencia = 	Math.pow(3, i);
-			
+			i++;
+			potencia = Math.pow(3, i);
 
 		} while (potencia < 12000);
 
