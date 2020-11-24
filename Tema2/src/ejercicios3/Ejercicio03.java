6c1d55633fc69bef7e179ace7b40ae30752a16ed
Carlos Sogorb <profesorderedes@gmail.com>
2020-11-24 19:20:50 +0100

    mar 24 nov 2020 19:20:50 CET

--- Tema2/src/ejercicios3/Ejercicio03.java
+++ Tema2/src/ejercicios3/Ejercicio03.java
@@ -1,40 +1,37 @@
 package ejercicios3;
+
 import java.util.Scanner;
 
 public class Ejercicio03 {
 
-    public static void main(String[] args) {
-       
-        Scanner entrada = new Scanner(System.in);
-       
-        while (true) {
-       
-            int num1 = (int) (Math.random() * 100 + 1);
-            int num2 = (int) (Math.random() * 100 + 1);
-            int suma;
-           
-            System.out.println(num1 + " y " + num2);
-           
-            System.out.println("¿Cuanto es " + num1 + " + " + num2 + "?");
-           
-            System.out.print("introduce el resultado: ");
-            suma = entrada.nextInt();
-           
-            while (suma != (num1 + num2)) {
-               
-                System.out.print("Incorrecto. Prueba otra vez: ");
-                suma = entrada.nextInt();
-               
-            }
-           
-            if (suma == (num1 + num2)) {
-               
-                System.out.println("Correcto.\n");
-               
-            }
-           
-        }
+	public static void main(String[] args) {
 
-    }
+		Scanner entrada = new Scanner(System.in);
+		int num1, num2, suma;
+
+		while (true) {
+
+			num1 = (int) (Math.random() * 100 + 1);
+			num2 = (int) (Math.random() * 100 + 1);
+
+			System.out.println("¿Cuánto es " + num1 + " + " + num2 + "?");
+
+			System.out.print("Introduce el resultado: ");
+			suma = entrada.nextInt();
+
+			if (suma == (num1 + num2)) {
+
+				System.out.println("Correcto.\n");
+
+			} else {
+
+				System.out.println("Has fallado, el resultado era " 
+						+ (num1 + num2) + ".\n");
+
+			}
+
+		}
+
+	}
 
 }
