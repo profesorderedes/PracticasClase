60a5f7ff8281ebca557ff3609b18458cefa5f123
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-01 20:02:34 +0100

    mar 01 dic 2020 20:02:34 CET

---
+++ Tema3/src/Ejer05.java
@@ -0,0 +1,30 @@
+public class Ejer05 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * 4.- Crea un array llamado companyeros con 4 posiciones y ve asignando a cada
+		 * 
+		 * una de ellas el nombre de uno de los cuatro compañeros más cercanos a tu
+		 * 
+		 * sitio. Haz que después se muestren todos los valores mediante un bucle for.
+		 * 
+		 */
+
+		String posiciones[] = new String[4];
+
+		posiciones[0] = "Jordi";
+		posiciones[1] = "Erik";
+		posiciones[2] = "Sergio";
+		posiciones[3] = "Jose";
+
+		for (int i = 0; i < posiciones.length; i++) {
+
+			System.out.println(posiciones[i]);
+
+		}
+
+	}
+
+}
