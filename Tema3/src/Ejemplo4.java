9180d14446bad3f7727bf40d8f17ac49a641114d
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-02 19:03:31 +0100

    mié 02 dic 2020 19:03:31 CET

---
+++ Tema3/src/Ejemplo4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejemplo4 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		// Array para almacenar edades de ingresados en un hospital.
+		int edades[] = new int[2000];
+		int edad;
+
+		// Leemos los datos del array. El usuario introducirá un valor 0
+		// para indicar que ha terminado de pasar los datos.
+		int i = 0;
+		do {
+
+			System.out.print("Edad " + i + ": ");
+			edad = entrada.nextInt();
+
+			edades[i] = edad;
+			i++;
+
+		} while (edad != 0);
+
+	}
+
+}
---
+++ Tema3/src/Ejer07.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejer07 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		double notas[] = new double[10];
+		double nota;
+		int i = 0;
+
+		while (i < notas.length) {
+
+			System.out.print("Nota " + (i + 1) + ": ");
+			nota = entrada.nextDouble();
+
+			notas[i] = nota;
+			i++;
+
+		}
+
+		System.out.println("\nNotas almacenadas:");
+		for (int j = 0; j < notas.length; j++) {
+
+			System.out.println(notas[j]);
+
+		}
+
+	}
+
+}
