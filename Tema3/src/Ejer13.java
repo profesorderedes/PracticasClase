11ec37f18bf09654b4661342ed025ce439f7abab
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-02 19:59:57 +0100

    mié 02 dic 2020 19:59:57 CET

---
+++ Tema3/src/Ejer13.java
@@ -0,0 +1,21 @@
+public class Ejer13 {
+
+	public static void main(String[] args) {
+
+		int potencias[] = new int[20];
+		int potencia = 1;
+
+		for (int i = 0; i < potencias.length; i++) {
+
+			potencia = potencia * 2;
+			potencias[i] = potencia;
+
+		}
+
+		for (int i = 0; i < potencias.length; i++) {
+			System.out.println(potencias[i]);
+		}
+
+	}
+
+}
