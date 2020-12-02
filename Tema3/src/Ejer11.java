f7c74401208c27983053871f7b3bd641b0427034
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-02 19:42:03 +0100

    mié 02 dic 2020 19:42:03 CET

--- Tema3/src/Ejer11.java
+++ Tema3/src/Ejer11.java
@@ -2,34 +2,37 @@ import java.util.Scanner;
 
 public class Ejer11 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
+		Scanner entrada = new Scanner(System.in);
 
-        String ordenes[] = new String[1000];
-        String orden;
-       
-        System.out.println("Escribe una de estas órdenes: izquierda, derecha, arriba, abajo, matar");
-        orden = entrada.nextLine();
-       
-        int i= 0;
-        while (!orden.equals("ejecutar")) {
-           
-            System.out.print("Escribe la orden: ");
-            orden = entrada.nextLine();
-           
-            ordenes[i] = orden;
-            i++;
-        }
-        
-        i=0;
-    
-        System.out.println("\nListado de comandos: ");
-       
-        while(!ordenes[i].equals("ejecutar")) {
-            System.out.println(ordenes[i]);
-            i++;
-        }
-    }
+		String ordenes[] = new String[1000];
+		String orden;
+
+		// Leemos los datos.
+		System.out.println("Escribe una de estas órdenes: izquierda, derecha, arriba, abajo, matar");
+		orden = entrada.nextLine();
+		ordenes[0] = orden;
+
+		int i = 1;
+		while (!orden.equals("ejecutar")) {
+
+			System.out.print("Escribe la orden: ");
+			orden = entrada.nextLine();
+
+			ordenes[i] = orden;
+			i++;
+		}
+
+		// Mostramos los datos.
+		i = 0;
+
+		System.out.println("\nListado de comandos: ");
+
+		while (!ordenes[i].equals("ejecutar")) {
+			System.out.println(ordenes[i]);
+			i++;
+		}
+	}
 
 }
