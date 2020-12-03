79641ca1a5f3c215d214b893ddc4077050ff471e
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-03 16:11:05 +0100

    jue 03 dic 2020 16:11:05 CET

---
+++ Tema3/src/Ejer17.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Ejer17 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * Escribe una aplicación que pida al usuario diez precios de productos. Después
+		 * 
+		 * devolverá el precio total a pagar y el número de productos cuyo precio sea
+		 * 
+		 * mayor de 10 euros.
+		 * 
+		 */
+
+		Scanner entrada = new Scanner(System.in);
+		double precios[] = new double[10];
+		double precioTotal = 0;
+		int mayores = 0;
+
+		for (int i = 0; i < precios.length; i++) {
+
+			System.out.print("Precio " + (i + 1) + " : ");
+			double precio = entrada.nextDouble();
+
+			precios[i] = precio;
+
+			precioTotal = precioTotal + precio;
+
+			if (precio > 10) {
+
+				mayores++;
+
+			}
+
+		}
+
+		System.out.println(
+				"El precio total es " + precioTotal + " y el número de productos mayor de 10 euros es " + mayores);
+
+	}
+
+}
