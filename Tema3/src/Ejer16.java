25e5098f01832062355324ebc8d4fb6bc27e8363
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-03 16:01:28 +0100

    jue 03 dic 2020 16:01:28 CET

--- Tema3/src/Ejer16.java
+++ Tema3/src/Ejer16.java
@@ -2,8 +2,7 @@ public class Ejer16 {
 
 	public static void main(String[] args) {
 
-		double temperaturas[] = { 15, 16.1, 16, 15.4, 15, 14, 10.2, 11, 
-				11.5, 10, 10.1, 9.8, 9, 6 };
+		double temperaturas[] = { 15, 16.1, 16, 15.4, 15, 14, 10.2, 11, 11.5, 10, 10.1, 9.8, 9, 6 };
 
 		double media = 0;
 		int tempAltas = 0;
@@ -20,26 +19,17 @@ public class Ejer16 {
 		media = media / 14;
 
 		System.out.println("\nTemperatura media: " + media + " ºC");
-		
-		for(int i=0; i<temperaturas.length; i++) {
-			
-			if(temperaturas[i]>12) {
+
+		for (int i = 0; i < temperaturas.length; i++) {
+
+			if (temperaturas[i] > 12) {
 				tempAltas++;
 			}
-			
+
 		}
-		
-		System.out.println("Hay " + tempAltas 
-				+ " temperaturas mayores de 12 ºC.");
+
+		System.out.println("Hay " + tempAltas + " temperaturas mayores de 12 ºC.");
 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
