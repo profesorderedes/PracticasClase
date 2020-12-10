6c163d6df2af9a486b1459d60069465c95879123
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-10 16:16:48 +0100

    jue 10 dic 2020 16:16:48 CET

--- Tema3/src/Ejemplo7.java
+++ Tema3/src/Ejemplo7.java
@@ -1,29 +1,28 @@
 
-// Cálculo del mínimo de un array.
+// Cálculo del máximo de un array.
 
 public class Ejemplo7 {
 
 	public static void main(String[] args) {
 
 		int notas[] = { 7, 1, 9, 3, 0, 10 };
-		int posMin = 0; // Posición del mínimo del array
-		
-		System.out.println("posMin = 0");
+		int posMax = 0; // Posición del mínimo del array
+
+		System.out.println("posMax = 0");
 
 		for (int i = 1; i < notas.length; i++) {
 
-			if (notas[i] < notas[posMin]) {
+			if (notas[i] > notas[posMax]) {
 
-				posMin = i;
-				System.out.println("posMin = " + i);
+				posMax = i;
+				System.out.println("posMax = " + i);
 
 			}
 
 		}
-		
-		System.out.println("\nEl elemento más pequeño del array se "
-				+ "encuentra en \nla posición " + posMin 
-				+ " y su valor es " + notas[posMin] + ".");
+
+		System.out.println("\nEl elemento más grande del array se " + "encuentra en \nla posición " + posMax
+				+ " y su valor es " + notas[posMax] + ".");
 
 	}
 
---
+++ Tema3/src/hoja2/Ejer5.java
@@ -0,0 +1,34 @@
+package hoja2;
+
+public class Ejer5 {
+
+	public static void main(String[] args) {
+
+		double temp[] = { 15, 16.1, 16, 15.4, 15, 14, 10.2, 11, 11.5, 10, 10.1, 9.8, 9, 6 };
+
+		int posMin = 0;
+		int posMax = 0;
+
+		for (int i = 0; i < temp.length; i++) {
+
+			if (temp[i] < temp[posMin]) {
+
+				posMin = i;
+
+			}
+
+			if (temp[i] > temp[posMax]) {
+
+				posMax = i;
+
+			}
+
+		}
+
+		System.out.println("La temperatura más alta es: " + temp[posMax] + "ºC, en la posición " + posMax);
+
+		System.out.println("La temperatura más Baja es: " + temp[posMin] + "ºC, en la posición " + posMin);
+
+	}
+
+}
