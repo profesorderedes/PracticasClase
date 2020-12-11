76b959844acd9e430fd65e354559f68e631cec57
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-11 20:13:17 +0100

    vie 11 dic 2020 20:13:16 CET

---
+++ Tema3/src/Ejemplo8.java
@@ -0,0 +1,14 @@
+
+// Algoritmo de selección para ordenar un array.
+
+public class Ejemplo8 {
+	
+	public static void main(String[] args) {
+		
+		int numeros[] = {9, 4, 7, 6, 1, 8};
+		int aux; // Variable para intercambiar los valores de dos posiciones
+		
+		
+	}
+
+}
