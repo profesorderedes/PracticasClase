79e52366eb9e2b68e25a4d4570f056a332d1a3e8
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-15 17:41:51 +0100

    mar 15 dic 2020 17:41:51 CET

--- Tema3/src/Ejemplo8.java
+++ Tema3/src/Ejemplo8.java
@@ -2,13 +2,28 @@
 // Algoritmo de selección para ordenar un array.
 
 public class Ejemplo8 {
-	
+
 	public static void main(String[] args) {
-		
-		int numeros[] = {9, 4, 7, 6, 1, 8};
+
+		int numeros[] = { 9, 4, 7, 6, 1, 8 };
 		int aux; // Variable para intercambiar los valores de dos posiciones
-		
-		
+		int posMin;
+
+		// Recorremos el array desde la primera posición hasta la penúltima.
+		// La última posición estará ordenada cuando el resto del array lo esté.
+		for (int i = 0; i < numeros.length - 1; i++) {
+
+			// La variable i nos indica desde qué punto del array hay que empezar
+			// a buscar el mínimo.
+
+			// Vamos a buscar el mínimo entre i y el final del array.
+			posMin = i;
+			for (int j = 0; j < numeros.length; j++) {
+
+			}
+
+		}
+
 	}
 
 }
