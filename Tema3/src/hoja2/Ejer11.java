bd797bd42a4961d7498fc5c2ec5368228af78c61
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-16 19:50:21 +0100

    mié 16 dic 2020 19:50:21 CET

---
+++ Tema3/src/hoja2/Ejer11.java
@@ -0,0 +1,48 @@
+package hoja2;
+
+import java.util.Scanner;
+
+public class Ejer11 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		int edades[] = new int[10];
+
+		int edad;
+		int posMin;
+		int aux;
+
+		for (int x = 0; x < edades.length; x++) {
+
+			System.out.println("Edad " + (x + 1) + ": ");
+			edad = entrada.nextInt();
+
+			edades[x] = edad;
+		}
+
+		for (int i = 0; i < edades.length; i++) {
+
+			posMin = i;
+			for (int j = i + 1; j < edades.length; j++) {
+
+				if (edades[j] < edades[posMin]) {
+					posMin = j;
+				}
+
+			}
+
+			aux = edades[i];
+			edades[i] = edades[posMin];
+			edades[posMin] = aux;
+
+		}
+
+		System.out.println("Edades ordenadas:");
+		for (int i = 0; i < edades.length; i++) {
+			System.out.print(edades[i] + " ");
+		}
+
+	}
+}
