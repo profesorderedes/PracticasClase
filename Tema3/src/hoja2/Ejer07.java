db3c01dd9767d15a7516317cb840b25614e12d33
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-16 19:53:36 +0100

    mié 16 dic 2020 19:53:36 CET

--- Tema3/src/hoja2/Ejer07.java
+++ Tema3/src/hoja2/Ejer07.java
@@ -4,8 +4,7 @@ public class Ejer07 {
 
 	public static void main(String[] args) {
 
-		int audiencia[] = { 2000000, 2500000, 3100000, 3000000, 1200000, 
-				1800000, 2050000 };
+		int audiencia[] = { 2000000, 2500000, 3100000, 3000000, 1200000, 1800000, 2050000 };
 
 		int posMin = 0;
 		int posMax = 0;
@@ -22,10 +21,8 @@ public class Ejer07 {
 
 		}
 
-		System.out.println("La audiencia más alta es: " + audiencia[posMax] 
-				+ ", en la posición " + posMax);
-		System.out.println("La audiencia más baja es: " + audiencia[posMin] 
-				+ ", en la posición " + posMin);
+		System.out.println("La audiencia más alta es: " + audiencia[posMax] + ", en la posición " + posMax);
+		System.out.println("La audiencia más baja es: " + audiencia[posMin] + ", en la posición " + posMin);
 
 	}
 
--- Tema3/src/hoja2/Ejer08.java
+++ Tema3/src/hoja2/Ejer08.java
@@ -4,8 +4,8 @@ public class Ejer08 {
 
 	public static void main(String[] args) {
 
-		double hum[] = { 45, 40, 40, 40, 37, 35, 39, 39, 45, 50, 55, 60, 65, 
-				70, 70, 75, 75, 72, 65, 60, 55, 50, 50, 45 };
+		double hum[] = { 45, 40, 40, 40, 37, 35, 39, 39, 45, 50, 55, 60, 65, 70, 70, 75, 75, 72, 65, 60, 55, 50, 50,
+				45 };
 
 		int posMin = 0;
 		int posMax = 0;
@@ -26,11 +26,9 @@ public class Ejer08 {
 
 		}
 
-		System.out.println("El mayor valor de humedad es: " + hum[posMax] 
-				+ ", a las " + posMax);
+		System.out.println("El mayor valor de humedad es: " + hum[posMax] + ", a las " + posMax);
 
-		System.out.println("El menor valor de humedad es: " + hum[posMin] 
-				+ ", a las " + posMin);
+		System.out.println("El menor valor de humedad es: " + hum[posMin] + ", a las " + posMin);
 	}
 
 }
