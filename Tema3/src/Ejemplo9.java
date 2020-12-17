2c1c2b8ac7fad0e13f0e9fa0655e2b773805e5bc
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-17 16:15:21 +0100

    jue 17 dic 2020 16:15:20 CET

--- Tema3/src/Ejemplo9.java
+++ Tema3/src/Ejemplo9.java
@@ -12,8 +12,10 @@ public class Ejemplo9 {
 				};
 
 		// Mostrar la primera fila.
+		System.out.println("Fila 0:");
 		System.out.println(medidas[0][0] + " " + medidas[0][1] + " " + medidas[0][2]);
 
+		System.out.println("Fila 0:");
 		for (int col = 0; col < 3; col++) {
 			System.out.print(medidas[0][col] + " ");
 		}
@@ -21,12 +23,31 @@ public class Ejemplo9 {
 		
 		// Mostrar la fila "fila".
 		int fila = 3;
+		System.out.println("Fila " + fila + ":");
 		for(int col = 0; col< 3; col++) {
 			System.out.print(medidas[fila][col] + " ");
 		}
 		
-		
+		// Mostrar la columna 0.
+		System.out.println("Columna 0:");
+		System.out.println(medidas[0][0] + medidas[1][0] + medidas[2][0] 
+				+ medidas[3][0]);
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
