392e3e4b27b4f93bf279b60e5a1d3302cb157d4e
Carlos Sogorb <profesorderedes@gmail.com>
2020-12-17 16:44:03 +0100

    jue 17 dic 2020 16:44:03 CET

--- Tema3/src/Ejemplo9.java
+++ Tema3/src/Ejemplo9.java
@@ -1,16 +1,11 @@
 
 public class Ejemplo9 {
-	
+
 	public static void main(String[] args) {
-		
+
 		// double medidas[][] = new double[4][3];
-		double medidas[][] = {
-				{8.5, 1020, 40}, 
-				{14.2, 1020, 35},
-				{11, 1022, 45},
-				{7, 1023, 50}
-				};
-		
+		double medidas[][] = { { 8.5, 1020, 40 }, { 14.2, 1020, 35 }, { 11, 1022, 45 }, { 7, 1023, 50 } };
+
 		int fila, col;
 
 		// Mostrar la primera fila.
@@ -22,60 +17,30 @@ public class Ejemplo9 {
 			System.out.print(medidas[0][col] + " ");
 		}
 		System.out.println();
-		
+
 		// Mostrar la fila "fila".
 		fila = 3;
 		System.out.println("Fila " + fila + ":");
-		for(col = 0; col< medidas[0].length; col++) {
+		for (col = 0; col < medidas[0].length; col++) {
 			System.out.print(medidas[fila][col] + " ");
 		}
-		
+
 		// Mostrar la columna 0.
 		System.out.println("\nColumna 0:");
-		System.out.println(medidas[0][0] + " " + medidas[1][0] + " " 
-				+ medidas[2][0] + " " + medidas[3][0]);
+		System.out.println(medidas[0][0] + " " + medidas[1][0] + " " + medidas[2][0] + " " + medidas[3][0]);
 
 		System.out.println("\nColumna 0:");
-		for(fila = 0; fila < medidas.length; fila++) {
+		for (fila = 0; fila < medidas.length; fila++) {
 			System.out.println(medidas[fila][0]);
 		}
-		
+
 		// Mostrar la columna "col".
 		col = 1;
 		System.out.println("\nColumna " + col + ":");
-		for(fila = 0; fila < medidas.length; fila++) {
+		for (fila = 0; fila < medidas.length; fila++) {
 			System.out.println(medidas[fila][col]);
 		}
-		
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
