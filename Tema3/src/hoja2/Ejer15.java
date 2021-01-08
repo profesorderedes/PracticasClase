8a8de94483f89333722a19bf12b9984cdf5396a2
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-08 19:46:31 +0100

    vie 08 ene 2021 19:46:30 CET

---
+++ Tema3/src/hoja2/Ejer15.java
@@ -0,0 +1,68 @@
+package hoja2;
+
+public class Ejer15 {
+
+	public static void main(String[] args) {
+
+		String precios[][] = { { "Tablet Samsung", "120" }, { "Móvil Sony", "115" }, { "Ipad Mini", "290" },
+				{ "Móvil HTC", "95" } };
+		float precio;
+		float total = 0;
+
+		// Mostramos la tabla en pantalla.
+		for (int fila = 0; fila < precios.length; fila++) {
+
+			for (int col = 0; col < precios[0].length; col++) {
+
+				System.out.print(precios[fila][col] + "\t");
+
+			}
+
+			System.out.println();
+
+		}
+
+		// Averiguamos el precio total de los artículos.
+		for (int fila = 0; fila < precios.length; fila++) {
+
+			// Convierto el String de la fila i, columna 1, a float.
+			precio = Float.parseFloat(precios[fila][1]);
+
+			total = total + precio;
+
+		}
+
+		System.out.println("Precio total: " + total);
+		
+		int posMin = 0;
+		// Buscamos el menor y el mayor precio.
+		for (int fila = 0; fila < precios.length; fila++) {
+			
+			if(Float.parseFloat(precios[fila][1]) < 
+					Float.parseFloat(precios[posMin][1])) {
+				posMin = fila;
+			}
+			
+		}
+		
+		System.out.println("El artículo más barato es " + precios[posMin][0] 
+				+ ", que vale " + precios[posMin][1] + " euros.");
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
