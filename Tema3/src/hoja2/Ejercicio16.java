4b379b8dba8b6a58abb7a65c8a55ad11d90e95c0
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-12 19:02:30 +0100

    mar 12 ene 2021 19:02:30 CET

---
+++ Tema3/src/hoja2/Ejercicio16.java
@@ -0,0 +1,39 @@
+package hoja2;
+
+import java.util.Scanner;
+
+public class Ejercicio16 {
+
+	public static void main(String[] args) {
+		Scanner entras = new Scanner(System.in);
+
+		String notas[][] = { 
+				{ "Jose Luis Martínez", "7" }, 
+				{ "Antonia Ruiz", "9" }, 
+				{ "Marcos Ramírez", "10" },
+				{ "Juana Herranz", "4" }, 
+				{ "Alberto López", "7" } };
+
+		float nota;
+		float total = 0;
+
+		System.out.println("Alumno\t\t|  Nota");
+		System.out.println("================|=====");
+
+		for (int fila = 0; fila < notas.length; fila++) {
+
+			for (int col = 0; col < notas[0].length; col++) {
+
+				System.out.print(notas[fila][col] + " \t");
+
+			}
+			System.out.println();
+			nota = Float.parseFloat(notas[fila][1]);
+			total = total + nota;
+		}
+
+		System.out.println();
+		System.out.println("La nota media es: " + total / 5);
+
+	}
+}
