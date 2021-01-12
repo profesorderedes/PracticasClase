5ab261f2a68ef2994c1b182aa740f783e57b570d
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-12 19:59:14 +0100

    mar 12 ene 2021 19:59:14 CET

---
+++ Tema3/src/hoja2/Ejer19.java
@@ -0,0 +1,43 @@
+package hoja2;
+
+import java.util.Scanner;
+
+public class Ejer19 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		String nominas[][] = new String[4][3];
+
+		// Leemos el apellido, nombre y sueldo de cada uno de los cuatro
+		// empleados. Estos tres datos formarán una fila de la tabla.
+
+		for (int fila = 0; fila < nominas.length; fila++) {
+
+			System.out.println("Empleado " + (fila + 1));
+			System.out.println("================");
+
+			System.out.print("Apellido: ");
+			nominas[fila][0] = entrada.nextLine();
+
+			System.out.print("Nombre: ");
+			nominas[fila][1] = entrada.nextLine();
+
+			System.out.print("Sueldo: ");
+			nominas[fila][2] = entrada.nextLine();
+
+			System.out.println();
+
+		}
+		
+		// Mostramos los datos en la pantalla.
+		for (int fila = 0; fila < nominas.length; fila++) {
+			
+			System.out.println(nominas[fila][0] + "\t" + nominas[fila][1] 
+					+ "\t" + nominas[fila][2]);
+			
+		}
+
+	}
+
+}
