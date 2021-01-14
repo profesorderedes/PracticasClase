bfab6006389fdd20fa361057ce56df574e24bf25
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-14 16:56:47 +0100

    jue 14 ene 2021 16:56:47 CET

---
+++ Tema3/src/ActividadesCadenas.java
@@ -0,0 +1,17 @@
+
+public class ActividadesCadenas {
+
+	public static void main(String[] args) {
+
+		// Método length()
+		String palabra = "león";
+		System.out.println("La palabra " + palabra + " tiene " 
+				+ palabra.length() + " caracteres.");
+
+		// Método charAt()
+		System.out.println("La primera letra de la palabra " + palabra 
+				+ " es " + palabra.charAt(0) + ".");
+		
+	}
+
+}
