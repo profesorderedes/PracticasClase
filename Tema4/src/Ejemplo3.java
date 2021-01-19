f40218499b452ad489262eaeea50b6fbc8193cb4
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-19 19:50:34 +0100

    mar 19 ene 2021 19:50:34 CET

--- Tema4/src/Ejemplo3.java
+++ Tema4/src/Ejemplo3.java
@@ -1,15 +1,22 @@
 
 public class Ejemplo3 {
-	
+
 	public static void main(String[] args) {
-		
+
 		mostrar("Carlos");
-		
+		doble(100);
+
 	}
-	
+
 	static void mostrar(String frase) {
-		
+
 		System.out.println(frase);
+
+	}
+
+	static void doble(int num) {
+		
+		System.out.println("El doble de " + num + " es " + num * 2);
 		
 	}
 
