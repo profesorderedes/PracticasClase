f3a4404a08c9e00918785ff78ea5d2b1355e7394
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-22 16:44:37 +0100

    vie 22 ene 2021 16:44:37 CET

--- Tema4/src/Ejemplo4.java
+++ Tema4/src/Ejemplo4.java
@@ -1,45 +1,45 @@
 
 public class Ejemplo4 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("El cuadrado de 5 es " + cuadrado(5));
 		System.out.println("El cubo de 10 es " + cubo(10));
-		
+		System.out.println("El precio final de un producto de 20 euros, "
+				+ "con un descuento del 10% y un IVA del 16% es " 
+				+ precioVenta(20, 10, 16));
+
 	}
-	
-	static int cuadrado (int num) {
-		
+
+	static int cuadrado(int num) {
+
 		int cuadrado = num * num;
 		return cuadrado;
-		
+
 	}
-	
+
 	static int cubo(int num) {
-		
+
 		return num * num * num;
-		
+
 	}
-	
+
 	// Calcular el precio descontado, aplicarle el IVA y devolver el precio
 	// final que tiene que pagar el cliente.
 	// precioVenta(precio, descuento, iva)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	static float precioVenta(float precio, float descuento, int iva) {
+		
+		
 
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
