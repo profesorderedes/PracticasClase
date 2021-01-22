0deda9476962d1d56c572868bf6866e43f04a1c8
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-22 17:05:17 +0100

    vie 22 ene 2021 17:05:17 CET

--- Tema4/src/Ejemplo4.java
+++ Tema4/src/Ejemplo4.java
@@ -6,8 +6,7 @@ public class Ejemplo4 {
 		System.out.println("El cuadrado de 5 es " + cuadrado(5));
 		System.out.println("El cubo de 10 es " + cubo(10));
 		System.out.println("El precio final de un producto de 20 euros, "
-				+ "con un descuento del 10% y un IVA del 16% es " 
-				+ precioVenta(20, 10, 16));
+				+ "con un descuento del 10% y un IVA del 16% es " + precioVenta(20, 10, 16));
 
 	}
 
@@ -18,7 +17,7 @@ public class Ejemplo4 {
 
 	}
 
-	static int cubo(int num) {
+	static double cubo(double num) {
 
 		return num * num * num;
 
@@ -28,18 +27,22 @@ public class Ejemplo4 {
 	// final que tiene que pagar el cliente.
 	// precioVenta(precio, descuento, iva)
 	static float precioVenta(float precio, float descuento, int iva) {
-		
-		
+
+		float precioDescontado = precio - precio * descuento / 100;
+		float precioFinal = precioDescontado + precioDescontado * iva / 100;
+
+		return precioFinal;
 
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	// Método que toma un número decimal y devuelve el volumen de una esfera con ese
+	// radio.
+	// volumenEsfera(radio)
+	// Volumen = 1.33 * 3.14 * radio * radio * radio
+	static double volumenEsfera(double radio) {
+
+		return 1.33 * 3.14 * cubo(radio);
+
+	}
+
 }
