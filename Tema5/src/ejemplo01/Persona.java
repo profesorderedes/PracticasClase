6e35d5554995086a0a56002169abd09749d954b7
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-29 16:50:30 +0100

    vie 29 ene 2021 16:50:30 CET

---
+++ Tema5/src/ejemplo01/Persona.java
@@ -0,0 +1,19 @@
+package ejemplo01;
+
+public class Persona {
+
+	String nombre;
+	String apellidos;
+	String dni;
+	int anyoNacimiento;
+
+	void mostrar() {
+
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellidos);
+		System.out.println("DNI: " + dni);
+		System.out.println("Año de nacimiento: " + anyoNacimiento);
+
+	}
+
+}
