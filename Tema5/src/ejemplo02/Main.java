21680a746dde4d56fe43860c88160ee33e56aa50
Carlos Sogorb <profesorderedes@gmail.com>
2021-01-29 19:27:02 +0100

    vie 29 ene 2021 19:27:02 CET

---
+++ Tema5/src/ejemplo02/Main.java
@@ -0,0 +1,17 @@
+package ejemplo02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Punto punto1 = new Punto();
+		
+		punto1.x = 50;
+		punto1.y = 25;
+		punto1.color = "Rojo";
+		
+		
+		
+	}
+	
+}
---
+++ Tema5/src/ejemplo02/Punto.java
@@ -0,0 +1,17 @@
+package ejemplo02;
+
+public class Punto {
+
+	int x; // Coordenada x del punto
+	int y; // Coordenada y del punto
+	String color; // Color del punto
+
+	void mostrar() {
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("Color = " + color);
+
+	}
+
+}
