167b52ced90e6c22269b606fc2e352c4fd3c8ad0
Carlos Sogorb <profesorderedes@gmail.com>
2021-02-02 19:16:18 +0100

    mar 02 feb 2021 19:16:18 CET

---
+++ Tema5/src/ejemplo04Rectangulo/Rectangulo.java
@@ -0,0 +1,19 @@
+package ejemplo04Rectangulo;
+
+public class Rectangulo {
+	
+	// Atributos
+	// (x,y) del vértice superior izquierdo
+	// Base, altura
+	
+	// Métodos 
+	// mostrar()
+	// area()
+	// perimetro()
+	// Después:
+	// mover(dx)
+	// estaDentro(x1,y1)
+	
+	
+
+}
