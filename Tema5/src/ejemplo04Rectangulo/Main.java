88fe5eaee7a53483cbbf8a117620ed1886c7b6b9
Carlos Sogorb <profesorderedes@gmail.com>
2021-02-02 19:32:53 +0100

    mar 02 feb 2021 19:32:53 CET

---
+++ Tema5/src/ejemplo04Rectangulo/Main.java
@@ -0,0 +1,18 @@
+package ejemplo04Rectangulo;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Rectangulo rect = new Rectangulo();
+		
+		rect.x = 20;
+		rect.y = 5;
+		rect.base = 30;
+		rect.altura = 10;
+		
+		rect.mostrar();
+		
+	}
+
+}
--- Tema5/src/ejemplo04Rectangulo/Rectangulo.java
+++ Tema5/src/ejemplo04Rectangulo/Rectangulo.java
@@ -8,12 +8,25 @@ public class Rectangulo {
 	
 	// Métodos 
 	// mostrar()
-	// area()
-	// perimetro()
-	// Después:
-	// mover(dx)
-	// estaDentro(x1,y1)
+	// area(): Devuelve el área del rectángulo
+	// perimetro(): Devuelve el perímetro del rectángulo
 	
+	
+	// Después:
+	// mover(dx): Desplaza el rectángulo en horizontal
+	// estaDentro(x1,y1): Devuelve true si el punto (x1,y1) está dentro
+	// 					  del rectángulo.
+	double x, y;
+	double base, altura;
+	
+	void mostrar() {
+		
+		System.out.println("Coordenadas vértice superior izquierdo: (" + x 
+				+ "," + y + ")");
+		System.out.println("Base: " + base);
+		System.out.println("Altura: " + altura);
+		
+	}
 	
 
 }
