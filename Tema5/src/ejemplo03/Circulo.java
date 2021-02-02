576bee7bb0ebfcc73286831b2c61261eaf9141ec
Carlos Sogorb <profesorderedes@gmail.com>
2021-02-02 19:06:34 +0100

    mar 02 feb 2021 19:06:33 CET

---
+++ Tema5/src/ejemplo03/Circulo.java
@@ -0,0 +1,43 @@
+package ejemplo03;
+
+public class Circulo {
+
+	// Atributos
+	// x,y (posición del centro)
+	// radio
+
+	// Métodos
+	// mostrar()
+	// area(): Devuelve el área
+	// perimetro: Devuelve el perímetro
+
+	double x, y; // Posición del centro
+	double radio; // Radio del círculo
+
+	void mostrar() {
+		System.out.println("Coordenadas del centro: (" + x + "," + y + ").");
+		System.out.println("Radio: " + radio + ".");
+	}
+
+	double area() {
+		return Math.PI * radio * radio;
+	}
+
+	double perimetro() {
+		return 2 * Math.PI * radio;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tema5/src/ejemplo03/Main.java
@@ -0,0 +1,25 @@
+package ejemplo03;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Circulo circulo1 = new Circulo();
+
+		circulo1.x = 10;
+		circulo1.y = 15;
+		circulo1.radio = 5;
+
+		circulo1.mostrar();
+
+		double superficie = circulo1.area();
+
+		System.out.println("\nEl área del círculo es: " + superficie);
+
+		double perimetro = circulo1.perimetro();
+
+		System.out.println("\nEl perímetro del círculo es: " + perimetro);
+		
+	}
+
+}
