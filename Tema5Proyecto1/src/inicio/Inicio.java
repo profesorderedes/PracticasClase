41ccea83546c3f4370a983b81f6abe687708aa4b
Carlos Sogorb <profesorderedes@gmail.com>
2021-02-10 19:52:47 +0100

    mié 10 feb 2021 19:52:47 CET

--- Tema5Proyecto1/src/inicio/Inicio.java
+++ Tema5Proyecto1/src/inicio/Inicio.java
@@ -1,32 +1,43 @@
 package inicio;
 
+import java.util.Scanner;
+
 import geometria.Cilindro;
 
 public class Inicio {
-	
+
 	public static void main(String[] args) {
-		
-		Cilindro Cilindro1 = new Cilindro();
-		
-		Cilindro1.setAltura(12);
-		Cilindro1.setRadio(8);
-		
-		double area = Cilindro1.getArea();
-		double volumen = Cilindro1.getVolumen();
-		
+
+		Scanner entrada = new Scanner(System.in);
+		int altura, radio;
+
+		System.out.println("CALCULADOR DE CILINDROS");
+		System.out.println("=======================");
+		System.out.println("Escribe la altura del cilindro: ");
+		altura = entrada.nextInt();
+
+		System.out.println("Escribe el radio del cilindro: ");
+		radio = entrada.nextInt();
+
+		Cilindro cilindro1 = new Cilindro(altura, radio);
+
+		double area = cilindro1.getArea();
+		double volumen = cilindro1.getVolumen();
+
 		System.out.println("El área del cilindro 1 es: " + area);
 		System.out.println("El volumen del cilindro 1 es: " + volumen);
-		
+		System.out.println("\nDatos del cilindro: \n" + cilindro1.toString());
+
 		Cilindro cilindro2 = new Cilindro();
-		
+
 		cilindro2.setAltura(10);
 		cilindro2.setRadio(5);
-		
+
 		System.out.println("\nEl area del cilindro2 es: " + cilindro2.getArea());
 		System.out.println("El volumen del cilindro2 es: " + cilindro2.getVolumen());
-		
+
 		System.out.println("\nDatos del cilindro2: \n" + cilindro2.toString());
-		
+
 	}
-	
+
 }
