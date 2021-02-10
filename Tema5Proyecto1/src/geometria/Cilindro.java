05598e7338373cf767d9e939208ace9819c71a1b
Carlos Sogorb <profesorderedes@gmail.com>
2021-02-10 19:33:39 +0100

    mié 10 feb 2021 19:33:37 CET

--- Tema5Proyecto1/src/geometria/Cilindro.java
+++ Tema5Proyecto1/src/geometria/Cilindro.java
@@ -1,5 +1,49 @@
 package geometria;
 
 public class Cilindro {
+	
+	private int altura;
+	private int radio;
+	private double PI = 3.141592;
+	
+	public Cilindro(){
+		
+	}
+	
+	public Cilindro(int altura, int radio){
+		
+	}
 
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getRadio() {
+		return radio;
+	}
+
+	public void setRadio(int radio) {
+		this.radio = radio;
+	}
+	
+	public double getArea() {
+		return 2 * PI * radio * (radio + altura); 
+	}
+	
+	public double getVolumen() {
+		return PI * (radio * radio) * altura;
+	}
+
+	public String toString() {
+		return "[altura=" + altura + ", radio=" + radio + "]";
+	}
+	
+
+	
+
+	
 }
--- Tema5Proyecto1/src/inicio/Inicio.java
+++ Tema5Proyecto1/src/inicio/Inicio.java
@@ -1,9 +1,32 @@
 package inicio;
 
+import geometria.Cilindro;
+
 public class Inicio {
 	
 	public static void main(String[] args) {
 		
+		Cilindro Cilindro1 = new Cilindro();
+		
+		Cilindro1.setAltura(12);
+		Cilindro1.setRadio(8);
+		
+		double area = Cilindro1.getArea();
+		double volumen = Cilindro1.getVolumen();
+		
+		System.out.println("El area del cilindro1 es: " + area);
+		System.out.println("El volumen del cilindro1 es: " + volumen);
+		
+		Cilindro Cilindro2 = new Cilindro();
+		
+		Cilindro2.setAltura(10);
+		Cilindro2.setRadio(5);
+		
+		System.out.println("\nEl area del cilindro2 es: " + area);
+		System.out.println("El volumen del cilindro2 es: " + volumen);
+		
+		System.out.println("\nDatos del cilindro2 " + Cilindro2.toString());
+		
 	}
-
+	
 }
