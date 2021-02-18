3c925c8cd58a03dd475fb36183943c45b6479bad
Carlos Sogorb <profesorderedes@gmail.com>
2021-02-18 16:51:19 +0100

    jue 18 feb 2021 16:51:19 CET

--- Tema5Proyecto8/src/auxiliar/Alumno.java
+++ Tema5Proyecto8/src/auxiliar/Alumno.java
@@ -22,67 +22,30 @@ public class Alumno {
 
 	public String toString() {
 
-		return "Núm. expediente: " + numExpediente + ", " + nombre + " " 
-				+ apellidos + ", grupo: " + grupo.getCurso()
+		return "Núm. expediente: " + numExpediente + ", " + nombre + " " + apellidos + ", grupo: " + grupo.getCurso()
 				+ "º " + grupo.getNivel() + " " + grupo.getClase();
 
 	}
-	
+
 	public void leeDatos() {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.print("Número de expediente: ");
 		numExpediente = entrada.nextInt();
-		
+
 		entrada.nextLine();
-		
+
 		System.out.print("Apellidos: ");
 		apellidos = entrada.nextLine();
-		
+
 		System.out.print("Nombre: ");
 		nombre = entrada.nextLine();
-		
-		System.out.print("Grupo: ");
-		
-		System.out.print("Id del grupo: ");
-		int id = entrada.nextInt();
-		
-		entrada.nextLine();
-		
-		System.out.print("Nivel del grupo: ");
-		String nivel = entrada.nextLine();
-		
-		System.out.print("Clase del grupo: ");
-		String clase = entrada.nextLine();
-		
-		System.out.print("Curso del grupo: ");
-		int curso = entrada.nextInt();
-		
-		Grupo grupo1 = new Grupo(id,nivel,curso,clase);
-		grupo = grupo1;
-		
+
+		System.out.println("Grupo: ");
+
+		grupo = new Grupo();
+		grupo.leeDatos(); 
 	}
 
-	
-	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
