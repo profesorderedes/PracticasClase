0dff7527dda194b6da1d994ce6ea551521665680
Carlos Sogorb <profesorderedes@gmail.com>
2021-02-18 16:56:18 +0100

    jue 18 feb 2021 16:56:18 CET

--- Tema5Proyecto8/src/auxiliar/Alumno.java
+++ Tema5Proyecto8/src/auxiliar/Alumno.java
@@ -46,6 +46,7 @@ public class Alumno {
 
 		grupo = new Grupo();
 		grupo.leeDatos(); 
+		
 	}
 
 }
--- Tema5Proyecto8/src/instituto/Instituto.java
+++ Tema5Proyecto8/src/instituto/Instituto.java
@@ -1,5 +1,13 @@
 package instituto;
 
+import auxiliar.Alumno;
+
 public class Instituto {
+	
+	private final int MAX_ALUMNOS = 1000;
+	
+	private Alumno alumnos[] = new Alumno[MAX_ALUMNOS];
+	
+	
 
 }
